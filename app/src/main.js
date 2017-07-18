6ab967bf8d0bfbcaf79f530698499b60e7f5efe7
김현석 <dragmove@naver.com>
2017-07-18 18:31:25 +0900

    Task : complete test immutable.js intro page code

--- app/src/main.js
+++ app/src/main.js
@@ -1,12 +1,13 @@
+// original document : https://facebook.github.io/immutable-js/
+// translate : https://medium.com/@kimyongin/immutable-js-2bdf0b66489a
+
 import Immutable from 'immutable';
 
 (function ($) {
   'use strict';
 
-  // https://medium.com/@kimyongin/immutable-js-2bdf0b66489a
-  // TODO - lazy seq
-
-  const Map = Immutable.Map;
+  const Map = Immutable.Map,
+    Seq = Immutable.Seq;
 
   $(document).ready(init);
 
@@ -22,7 +23,13 @@ import Immutable from 'immutable';
 
     nested();
 
-    // TODO - lazy seq
+    lazySeq();
+
+    changeMapToLazySeq();
+
+    equal();
+
+    mutation();
   }
 
   function map() {
@@ -83,5 +90,48 @@ import Immutable from 'immutable';
 
     var nested4 = nested3.updateIn(['a', 'b', 'c'], list => list.push(6));
     console.log('nested4 :', nested4); // Map { a: Map { b: Map { c: List [ 3, 4, 5, 6 ], d: 7 } } }
+  }
+
+  function lazySeq() {
+    console.log('Seq.of(1, 2, 3, 4, 5, 6, 7, 8, 9) :', Seq.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
+    var oddSquares = Seq.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
+      .filter(x => {
+        console.log('seq filter x :', x);
+        return x % 2;
+      })
+      .map(x => {
+        console.log('seq map x :', x);
+        return x * x;
+      });
+
+    console.log('oddSquares.get(0) :', oddSquares.get(0)); // 1
+    console.log('oddSquares.get(1) :', oddSquares.get(1)); // 9
+    console.log('oddSquares.get(2) :', oddSquares.get(2)); // 25
+  }
+
+  function changeMapToLazySeq() {
+    var seq = Map({a: 1, b: 2, c: 3}).toSeq();
+    console.log('changeMapToLazySeq - seq :', seq);
+  }
+
+  function equal() {
+    const map1 = Immutable.Map({a: 1, b: 2, c: 3});
+    const map2 = Immutable.Map({a: 1, b: 2, c: 3});
+
+    console.log('Immutable.is :', Immutable.is(map1, map2));
+    console.log('Immutable.equals :', map1.equals(map2));
+  }
+
+  function mutation() {
+    // you must use set, push, pop in withMutation function. (don't use map, filter, sort, splice, etc... in withMutation function.)
+
+    const list1 = Immutable.List.of(1, 2, 3);
+    const list2 = list1.withMutations(function(list) {
+      list.push(4).push(5).push(6);
+    });
+
+    console.log('mutation - list1.size :', list1.size);
+    console.log('mutation - list2.size :', list2.size);
   }
 }(jQuery));
